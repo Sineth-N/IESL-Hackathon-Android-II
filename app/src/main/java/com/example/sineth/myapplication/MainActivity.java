0c2624e5fd7ed7b1b51902c1e8d2ccdff712d4e1
Sineth-N <sinethneranjana@gmail.com>
2015-10-05 16:28:15 +0530

    Added a recycler view to view all the currently set alarms

--- app/src/main/java/com/example/sineth/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sineth/myapplication/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.sineth.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    //for a recycle view to work you need to have 3 components
+    //those are the above mentioned types of objects.
+    //adapter is for setting the data inside the recycler view
+    // layout manager handles the overall layout operations like
+    // recycling the views and making the UI more attractive
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        //this is the way we map whatever in the GUI of the screen to the
+        // java code lying here use findviewbyid method and cast to the needed
+        // type like i've done here.
+
+        layoutManager = new LinearLayoutManager(this);
+        //setting the linear layout of the view this will look like a normal list
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        //we need to set up the adapter here for the recycler view.
+        adapter = new RVAdapter(getDataset());
+        recyclerView.setAdapter(adapter);
+    }
+
+    //This method only provides the data for testing purposes
+    //should be dumped in real testing purposes;
+    private String[] getDataset() {
+        return new String[]{"a", "b", "c"};
     }
 
     @Override
